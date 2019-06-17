435d84741504bbf1e54a58f01137f3412b1c5ea3
szabolcsveer <futuremove@gmail.com>
2019-06-17 16:29:03 +0200

    a5

--- greet.js
+++ greet.js
@@ -1,13 +1,10 @@
-<<<<<<< HEAD
 function Greet(saysomething) {
     console.log(saysomething);
 }
 
 Greet("Greetings!");
-=======
 function Greet(saysomething,color){
   console.log(saysomething,color);
 }
 
 Greet("Hello Szabi","blue");
->>>>>>> Szabi-SZ1
